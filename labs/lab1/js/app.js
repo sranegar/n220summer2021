54413cdce18ed22a40f9500ec6124a4d3ee7e258
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-18 14:35:41 -0400

    adding more commenting to lab1

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -11,45 +11,45 @@ function setup() {
     // Line connecting red ball to head
 
     stroke(1);
-    strokeWeight (2);
-    line(220, 220, 60, 40,);
+    strokeWeight (2); //set thickness
+    line(220, 220, 60, 40,); //line going from top left to center
 
     // red ball on top of head
 
-    strokeWeight(defaultWeight);
-    noStroke();
-    fill(red);
-    circle(50,30,20);
+    strokeWeight(defaultWeight); 
+    noStroke(); //no stroke around red ball
+    fill(red); //dip paint in red
+    circle(50,30,20); //creates small circle
 
     // rectangle moogle wings because triangles are too hard
 
     strokeWeight(defaultWeight);
-    fill("#6F50C7");
+    fill("#6F50C7"); //dip paint in purple
     rect(12,130,280,100);
 
     
     // ears come first because they are layered behind the face
 
     strokeWeight(defaultWeight);
-    fill("#F4EFEF");
-    stroke("black");
-    circle(100,140,50);
+    fill("#F4EFEF"); //dip paint in light gray 
+    stroke("black"); 
+    circle(100,140,50); //color applied to each circle
     circle(200,140,50);
 
     // three circles to make up moogle face
 
-    fill("#DBD1D1");
-    stroke("black");
+    fill("#DBD1D1"); //dip paint in light gray
+    stroke("black"); 
     strokeWeight(defaultWeight);
 
-    circle(150,150,50);
+    circle(150,150,50); //paint applied to all three 
     circle(100,200,50);
     circle(200,200,50);
 
 
     // moogle nose!
 
-    fill(red);
+    fill(red); //dip paint in red
     noStroke();
     circle(150,220,50);
 
---
+++ labs/lab3/js/app.js
@@ -0,0 +1,26 @@
+var x = 100;
+var y = 100;
+
+
+function setup() {
+    createCanvas (400,400);
+}
+
+function draw () {
+    background(0);
+    circle(width-mouseX, height-mouseX, 30);
+    fill (245, 123, 66); 
+
+    // if (mouseX = x,y) {
+    //     x = -x; //Flip direction
+    // } 
+    // if (mouseY = x,y) {
+    //     y = -y; 
+    // }
+
+}
+
+//orange circle
+// x,y of circle
+//set circle opposite of where mouse is
+
