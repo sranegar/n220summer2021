d33c752757f615644f6daa479a7a9874e88ea19c
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-18 23:06:17 -0400

    updating commenting

--- labs/lab2/worldWrap/js/app.js
+++ labs/lab2/worldWrap/js/app.js
@@ -7,16 +7,16 @@ function setup () {
 
 function draw() {
     
-    background(0);
-    fill (157, 237, 237);
+    background(0); //assign background
+    fill (157, 237, 237); //assign color
 
-    xPos = xPos + 5;  // move ball to the right 5 px per frame
+    xPos = xPos + 5;  // move ball to the right on x-axis 5 px per frame
 
 
-    if(xPos >= 800) { // if ball is 800px or greater
-        xPos = yPos - 5; // move ball back to far left of screen
+    if(xPos > 800) { // stops ball when it reaches 800px
+        xPos = yPos - 5; // move ball back to far left of screen once it exceeds 800px
     } 
 
-    circle(xPos,300,50);
+    circle(xPos,300,50); //x position of circle = 0
 }
 
