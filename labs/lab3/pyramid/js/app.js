775c87de8f6b464a9a09da99eb6c30820ac2bd66
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-18 22:31:40 -0400

    updating commenting

--- labs/lab3/pyramid/js/app.js
+++ labs/lab3/pyramid/js/app.js
@@ -6,10 +6,13 @@ function draw() {
     fill ("red");
     noStroke ();
 
-    for (var s = 0; s < 4; s++) { 
-    for (var r = 0; r <= s; r++) {
-         rect(r*42, s*42, 40, 40);
+    for (var s = 0; s < 4; s++) {  //y axis (rects) cannot exceed 4
+    for (var r = 0; r <= s; r++) { //x axis cannot exceed y axis in order to create pyramid
+         rect(r*42, s*42, 40, 40); //x and y coordinates spaced out accordingly
     }
 }
-
 }
+
+//y axis cannot exceed four(rectangles)
+//y axis increases by one
+//x axis cannot exceed y axis 
