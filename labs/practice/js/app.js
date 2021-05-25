e01aeb8c394c22e688f0f86de871fc3945203a98
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-25 13:58:14 -0400

    push practice

--- labs/practice/js/app.js
+++ labs/practice/js/app.js
@@ -1,27 +1,16 @@
-let h = 50; // X offset
-let kl = 100; //Vertical Shift
-let pol = .1; //Angle (period)
-let r = 10; //scaler
-let j = 5; //circle spacing
-
+let oos = [ 15, 22, 10, 5 ];
+let ahs = [ "#ed1a80", "#fcba03", "#ed1a80", "#fcba03" ]
 
 function setup() {
-createCanvas(400, 400);
-
+  createCanvas(400, 400);
+  
+    translate(200,200);
+  background(50);
+  
+  for( let i = 0; i < oos.length; i++) { 
+    // console.log(ahs[i]);
+    fill(ahs[i]);
+    circle(sin(i) * 30, cos(i)* 40, oos[i]);
+  }
 }
-
-function draw()  {
-fill(255,0,0);
-background(220);
-
-//draws a sine curve
-for(let i = 0; i < 50; i++) { //increase circle on x axis by 1 49 times
-  let zf = Math.sin(i * pol) * r; //y location
-  circle(i * j + h, zf + kl, 20); //(0*10)+50, sf+100  //incrase x axis to the right, y axis 
-}
-
-zf = kl + 1; 
-
-}
-
 
