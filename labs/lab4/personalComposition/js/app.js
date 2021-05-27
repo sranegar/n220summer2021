49da4071f8726ffff12206f20c6f67ca069b8e51
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-27 12:27:14 -0400

    commenting

--- labs/lab4/personalComposition/js/app.js
+++ labs/lab4/personalComposition/js/app.js
@@ -1,5 +1,6 @@
-let myColors = ["#ab85ed", "#e485ed", "#ed85a9", "#eda885", "#eddc85", "#a4ed85", "#85edea", "#859ced"];
-let myCircles = [140,340,540,740,940,1140,1340];
+let myColors = ["#ab85ed", "#e485ed", "#ed85a9", "#eda885", "#eddc85", "#a4ed85", "#85edea", "#859ced"]; //Assign colors of rainbow
+let myCircles = [140,340,540,740,940,1140,1340]; //Assign values and use for x-axis 
+ 
 function setup() {
     createCanvas (1920,700);
     noStroke ();
@@ -11,9 +12,9 @@ function setup() {
         rect(i*200,i,80,700); //Create rectangles across the x axis
     }
 
-    for (var i = 0; i < myCircles.length; i++) { 
-        fill (200);
-        circle(myCircles[i], 375, 50);
+    for (var i = 0; i < myCircles.length; i++) { //Value cannot exceed amount of integers in the array
+        fill (200); //fill each circle a gray color
+        circle(myCircles[i], 375, 50); //move circles across the x-axis
     }
 }
 
