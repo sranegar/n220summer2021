195182e87668c76237399334250a2595eaeb10a2
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-31 14:38:42 -0400

    current peak

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,18 +1,21 @@
-let dvGreen= document.getElementById("green")
-let varHeight = "100px"
-let varWidth = "100px"
+let dvGreen= document.getElementById("green");
+let varHeight = "100px";
+let varWidth = "100px";
 
 
 dvGreen.style.backgroundColor = "#96f2ae"; 
-dvGreen.style.width = varWidth
-dvGreen.style.height = varHeight
+dvGreen.style.width = varWidth;
+dvGreen.style.height = varHeight;
 
 function increaseSize () {
-        if(dvGreen >= "100px") {
-              dvGreen.style.height = + "110px";
-              dvGreen.style.width = + "110px";
-        }
-        dvGreen.style.backgroundColor = "#F5021E";
+      let increaseWidth = "110px";
+      let increaseHeight = "110px";
+      if(increaseWidth >= varWidth) dvGreen.style.width = increaseWidth;
+      if (increaseHeight >= varHeight)
+      dvGreen.style.height= increaseHeight;
+
+      
+      dvGreen.style.backgroundColor = "#F5021E";
 }
 
 
