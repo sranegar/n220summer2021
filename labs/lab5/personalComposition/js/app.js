41a3626d43b1f7d7d43789909776f5d4c87a31ef
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-31 16:26:38 -0400

    personal comp updates

--- labs/lab5/personalComposition/js/app.js
+++ labs/lab5/personalComposition/js/app.js
@@ -1,20 +1,20 @@
-let dvCoin = document.getElementById("coin"); //call to ID to style div
+let dvCardFlip = document.getElementById("cardFlip"); //call to ID to style div
 
-dvCoin.innerHTML = "hello"; //set inner HTML to "hello"
-dvCoin.style.backgroundColor = "#0394fc" //style background color of div
-dvCoin.style.width = "200px"; //style width of div
-dvCoin.style.height = "200px"; //style height of div
+dvCardFlip.innerHTML = "hello"; //set inner HTML to "hello"
+dvCardFlip.style.backgroundColor = "#0394fc" //style background color of div
+dvCardFlip.style.width = "200px"; //style width of div
+dvCardFlip.style.height = "200px"; //style height of div
 
 function colorChange () { //style mouse over function
-    dvCoin.style.backgroundColor = "#ec03fc" //change div color to pink when mouse is inside div
+    dvCardFlip.style.backgroundColor = "#ec03fc" //change div color to pink when mouse is inside div
 }
 function resetColor () { //style mouse out function
-    dvCoin.style.backgroundColor = "#0394fc" //change div color to blue when mouse is outside the div
+    dvCardFlip.style.backgroundColor = "#0394fc" //change div color to blue when mouse is outside the div
 }
 
 function greetings() { //style inner HTML
     let flip = Math.random(); //variable set to random
     let side = "Goodbye"; //variable set to "Goodbye"
     if (flip > .5) side = "Hello" //style inner HTML to say "Hello" if Math.random is greater than .5
-    dvCoin.innerHTML = side; //style inner HTML to say "Goodbye" if the random number is less than .5
+    dvCardFlip.innerHTML = side; //style inner HTML to say "Goodbye" if the random number is less than .5
 }
