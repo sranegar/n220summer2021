2951d22a144d0f2efc6eb951ddd8171192998987
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-31 14:54:20 -0400

    peak pixel update

---
+++ labs/lab5/peakPixels/js/app.js
@@ -0,0 +1,30 @@
+let dvGreen= document.getElementById("green");
+let height = 100;
+height = height + 60;
+let width = 100;
+width = width + 60;
+
+
+dvGreen.style.backgroundColor = "#96f2ae"; 
+dvGreen.style.width = width + "px";
+dvGreen.style.height = height + "px";
+
+function increaseSize () {
+      dvGreen.style.width = width + ".1px";
+      dvGreen.style.height = height + ".1px";
+      if(height == width) dvGreen.style.width
+      if (height == width)
+      dvGreen.style.height
+      
+      dvGreen.style.backgroundColor = "#F5021E";
+}
+
+
+
+
+
+
+
+//100x100 square div in green
+//when clicked, increases by 10% each time
+//make variable to store height and width
