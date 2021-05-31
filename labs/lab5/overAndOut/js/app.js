b626927577bc41cbf3330422067d6432f832db4c
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-05-31 12:01:49 -0400

    Over and out exercise added

---
+++ labs/lab5/overAndOut/js/app.js
@@ -0,0 +1,13 @@
+let dvBox = document.getElementById("box")
+
+dvBox.style.backgroundColor = "#4990bf";
+dvBox.style.width = "100px";
+dvBox.style.height = "100px";
+
+function changeToBlack() {
+    dvBox.style.backgroundColor = "#000000";
+}
+
+function changeToBlue () {
+    dvBox.style.backgroundColor = "#4990bf";
+}
