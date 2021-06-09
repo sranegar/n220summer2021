0f25e3d4638a4b2c7870ca3a0009277c39036e10
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-06-09 18:48:24 -0400

    pushing tipCalculator

---
+++ labs/lab7/tipCalculator/js/app.js
@@ -0,0 +1,13 @@
+let billTotal = document.getElementById("billTotal"); //Pull input into js
+// console.log(txtName);
+let dvOutput = document.getElementById("dvOutput"); //Pull output div into js
+
+
+
+
+function generateTotalBill() { //Create function 
+    let tipPercentage = .2 * Number(billTotal.value); //Calculate tip percentage for each input
+    let finalTotal = tipPercentage + Number(billTotal.value); //Add the tip percentage to the input to get total bill
+    console.log("Tip: " + "$"+tipPercentage + "Total: " + "$"+finalTotal); //Console log tip percentage plus total bill
+    dvOutput.innerHTML = "Tip: " + "$"+tipPercentage + "Total: " + "$"+finalTotal; //Display in a div for user 
+}
