42551ce18716ded53f8257c9fa5b875d5cc25b68
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-06-16 20:36:20 -0400

    comments

--- labs/lab9/badWordCatcher/js/app.js
+++ labs/lab9/badWordCatcher/js/app.js
@@ -3,20 +3,20 @@ let insertTxt = document.getElementById ("insertTxt");
 
 let sum = 0
 
-let badWordArray = ["water", "clear", "tires"]
-
+let badWordArray = ["water", "clear", "tires"] //Create array of bad words
+ 
 
 function badWordCatch () {
 
-    let string = insertTxt.value;
+    let string = insertTxt.value; //turn input into a string
     console.log(string);
-    let splitString = string.split(" ");
+    let splitString = string.split(" "); //seperate on space
     console.log(splitString);
 
-    for (var i = 0; i < string.length; i++) {
-       if (string.includes(badWordArray[i])) {
-            sum = sum + 1;
-            txtBox.innerHTML = "Bad word(s) found: " + sum;
+    for (var i = 0; i < string.length; i++) { //loop through the length of the input array
+       if (string.includes(badWordArray[i])) { //if input array contains any of the strings from the badword array
+            sum = sum + 1;  //add one per string
+            txtBox.innerHTML = "Bad word(s) found: " + sum; //display in output 
         } 
     }
 
